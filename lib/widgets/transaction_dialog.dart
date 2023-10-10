f932e861c10bda1681a65415f759409c5679ba43
lz-coder <lzcoder@proton.me>
2023-10-10 02:12:23 -0300

    Create transactions

--- lib/widgets/transaction_dialog.dart
+++ lib/widgets/transaction_dialog.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:just_finance_app/db/database.dart';
 import 'package:just_finance_app/src/transaction_info.dart';
+
+var transactionsDb = TransactionsDatabase();
 
 class TransactionDialog extends StatefulWidget {
   final Function insertCallback;
@@ -16,7 +19,13 @@ class TransactionDialog extends StatefulWidget {
 }
 
 class _TransactionDialogState extends State<TransactionDialog> {
-  var value_controller = TextEditingController();
+  final valueController = TextEditingController();
+  final titleController = TextEditingController();
+
+  void closeDialog(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -24,6 +33,11 @@ class _TransactionDialogState extends State<TransactionDialog> {
         height: 400,
         width: 400,
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+          color: widget.incomming
+              ? const Color.fromARGB(155, 39, 80, 51)
+              : const Color.fromARGB(155, 80, 47, 39),
+        ),
         child: Column(
           children: [
             const Text(
@@ -31,13 +45,51 @@ class _TransactionDialogState extends State<TransactionDialog> {
               style: TextStyle(fontSize: 18),
             ),
             TextField(
+              controller: titleController,
+              decoration: const InputDecoration(label: Text('Title')),
+            ),
+            TextField(
               keyboardType: TextInputType.number,
-              controller: value_controller,
+              controller: valueController,
               decoration: const InputDecoration(label: Text('Value')),
             ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text('Add'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      late int lastTransactionId;
+                      final transactionsList =
+                          await transactionsDb.transactionsList();
+                      if (transactionsList.isNotEmpty) {
+                        lastTransactionId = transactionsList.last.id + 1;
+                      } else {
+                        lastTransactionId = 0;
+                      }
+                      final transaction = TransactionInfo(
+                        id: lastTransactionId,
+                        title: titleController.text.isNotEmpty
+                            ? titleController.text
+                            : 'New transaction',
+                        incomming: widget.incomming ? 1 : 0,
+                        value: valueController.text.isNotEmpty
+                            ? double.parse(valueController.text)
+                            : 1,
+                      );
+                      await widget.insertCallback(transaction);
+                      if (context.mounted) Navigator.of(context).pop();
+                    } catch (err) {
+                      debugPrint(err.toString());
+                    }
+                  },
+                  child: const Text('Add'),
+                ),
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Cancel'),
+                ),
+              ],
             ),
           ],
         ),
