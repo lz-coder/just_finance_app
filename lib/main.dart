a84744a49d7f1961ad696e8ce7008a9a891281f0
lz-coder <lzcoder@proton.me>
2023-10-12 01:31:18 -0300

    Initial categorie support

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:just_finance_app/main_app.dart';
+import 'package:just_finance_app/src/categorie.dart';
 
 import 'db/database.dart';
 
-final transactionsDb = TransactionsDatabase();
+final coreDatabase = CoreDatabase();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  coreDatabase.insertCategorie(
+      Categorie(id: 0, name: 'Salário', type: CategorieTypes.incomming));
+  coreDatabase.insertCategorie(
+      Categorie(id: 1, name: 'Extra', type: CategorieTypes.incomming));
+  var categories = await coreDatabase.incommingCategories();
+  print(categories);
+
   runApp(const MainApp());
 }
