4ed30b127e5a7c04311a9bfd1a1fa0ede87ee3cf
lz-coder <lzcoder@proton.me>
2023-10-20 15:44:05 -0300

    Move defaults categories creation to MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:just_finance_app/Repository/wallet_repository.dart';
 import 'package:just_finance_app/main_app.dart';
-import 'package:just_finance_app/src/categorie.dart';
 import 'package:provider/provider.dart';
 
 import 'db/database.dart';
@@ -10,15 +9,6 @@ final coreDatabase = CoreDatabase();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  coreDatabase.insertCategorie(
-      Categorie(id: 0, name: 'Salário', type: CategorieTypes.incomming));
-  coreDatabase.insertCategorie(
-      Categorie(id: 1, name: 'Extra', type: CategorieTypes.incomming));
-  coreDatabase.insertCategorie(
-      Categorie(id: 2, name: 'Contas', type: CategorieTypes.dispense));
-  coreDatabase.insertCategorie(
-      Categorie(id: 3, name: 'Gastos pessoais', type: CategorieTypes.dispense));
 
   runApp(ChangeNotifierProvider(
     create: (context) => WalletRepository(),
--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:just_finance_app/pages/home_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:just_finance_app/src/categorie.dart';
+import 'package:just_finance_app/db/database.dart';
+
+final coreDatabase = CoreDatabase();
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -10,9 +15,39 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Just Finance',
       theme: ThemeData(colorScheme: const ColorScheme.dark()),
-      home: const DefaultTabController(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: DefaultTabController(
         length: 2,
-        child: HomePage(),
+        child: Builder(builder: (context) {
+          coreDatabase.insertCategorie(
+            Categorie(
+                id: 0,
+                name: AppLocalizations.of(context)!.defaultCategorieSalary,
+                type: CategorieTypes.incomming),
+          );
+          coreDatabase.insertCategorie(
+            Categorie(
+                id: 1,
+                name: AppLocalizations.of(context)!.defaultCategorieExtra,
+                type: CategorieTypes.incomming),
+          );
+          coreDatabase.insertCategorie(
+            Categorie(
+                id: 2,
+                name: AppLocalizations.of(context)!.defaultCategorieBills,
+                type: CategorieTypes.dispense),
+          );
+          coreDatabase.insertCategorie(
+            Categorie(
+                id: 3,
+                name: AppLocalizations.of(context)!
+                    .defaultCategoriePersonalExpenses,
+                type: CategorieTypes.dispense),
+          );
+
+          return const HomePage();
+        }),
       ),
     );
   }
