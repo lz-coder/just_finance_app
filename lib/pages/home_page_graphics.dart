15d4ef04db808c18e8811682af3579ef5446971d
lz-coder <lzcoder@proton.me>
2023-10-20 04:13:30 -0300

    Animation speed

--- lib/pages/home_page_graphics.dart
+++ lib/pages/home_page_graphics.dart
@@ -20,32 +20,34 @@ class HomePageGraphics extends StatelessWidget {
     ];
     if (incommingValue > 0 || dispenseValue > 0) {
       return Center(
-          child: SfCircularChart(
-        title: ChartTitle(text: 'Gráfico geral'),
-        legend: const Legend(isVisible: true),
-        palette: const [
-          Color.fromARGB(204, 83, 117, 76),
-          Color.fromARGB(204, 117, 76, 76),
-        ],
-        series: <PieSeries<_PieData, String>>[
-          PieSeries<_PieData, String>(
-            explode: false,
-            explodeIndex: 0,
-            dataSource: pieData,
-            xValueMapper: (_PieData data, _) => data.xData,
-            yValueMapper: (_PieData data, _) => data.yData,
-            dataLabelMapper: (_PieData data, _) =>
-                '${data.text} \n \$\$ ${data.yData}',
-            dataLabelSettings: const DataLabelSettings(
-              isVisible: true,
-              textStyle: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+        child: SfCircularChart(
+          title: ChartTitle(text: 'Gráfico geral'),
+          legend: const Legend(isVisible: true),
+          palette: const [
+            Color.fromARGB(204, 83, 117, 76),
+            Color.fromARGB(204, 117, 76, 76),
+          ],
+          series: <PieSeries<_PieData, String>>[
+            PieSeries<_PieData, String>(
+              animationDuration: 750,
+              explode: false,
+              explodeIndex: 0,
+              dataSource: pieData,
+              xValueMapper: (_PieData data, _) => data.xData,
+              yValueMapper: (_PieData data, _) => data.yData,
+              dataLabelMapper: (_PieData data, _) =>
+                  '${data.text} \n \$\$ ${data.yData}',
+              dataLabelSettings: const DataLabelSettings(
+                isVisible: true,
+                textStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        ],
-      ));
+          ],
+        ),
+      );
     } else {
       return const Center(
         child: Text(
