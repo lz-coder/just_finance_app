29383ab3d04991e7d6de87682368fc4cc71811ff
lz-coder <lzcoder@proton.me>
2023-10-22 10:23:02 -0300

    change path to main_app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:just_finance_app/Repository/config_repository.dart';
 import 'package:just_finance_app/Repository/wallet_repository.dart';
-import 'package:just_finance_app/main_app.dart';
+import 'package:just_finance_app/src/main_app.dart';
 import 'package:provider/provider.dart';
 
 import 'db/database.dart';
