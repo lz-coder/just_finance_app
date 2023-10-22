35a35da53a7ae9fdca5cbfe686a7b9453773ac30
lz-coder <lzcoder@proton.me>
2023-10-22 10:26:06 -0300

    Add changeCurrentLocale method

--- lib/Repository/config_repository.dart
+++ lib/Repository/config_repository.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:just_finance_app/db/database.dart';
+
+final coreDatabase = CoreDatabase();
 
 class ConfigRepository extends ChangeNotifier {
   Locale? _currentLocale;
 
   set currentLocale(Locale? value) {
     _currentLocale = value;
+    notifyListeners();
+  }
+
+  Future<void> changeCurrentLocale(Locale? locale) async {
+    _currentLocale = locale;
+    await coreDatabase.updateConfig(
+        'locale_config', '${locale!.languageCode}_${locale.countryCode}');
     notifyListeners();
   }
 
