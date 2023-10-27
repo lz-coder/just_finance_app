553a77ce32ff7675ffd557a37ac279753f9a7715
lz-coder <lzcoder@proton.me>
2023-10-27 12:11:21 -0300

    Updates transactions according to category management

--- lib/pages/home_page_transactions.dart
+++ lib/pages/home_page_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_finance_app/Repository/category_repository.dart';
 import 'package:just_finance_app/Repository/wallet_repository.dart';
 import 'package:just_finance_app/db/database.dart';
 import 'package:just_finance_app/widgets/transaction_card.dart';
@@ -17,50 +18,55 @@ class HomePageTransactions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: coreDatabase.transactionsList(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            padding: const EdgeInsets.only(bottom: 90),
-            itemCount: snapshot.data!.length >= _viewCount
-                ? _viewCount
-                : snapshot.data!.length,
-            itemBuilder: (context, index) {
-              // reverse the order on the list
-              final transaction =
-                  snapshot.data![snapshot.data!.length - index - 1];
-              return Dismissible(
-                key: UniqueKey(),
-                direction: DismissDirection.endToStart,
-                onDismissed: (direction) async {
-                  await Provider.of<WalletRepository>(context, listen: false)
-                      .removeTransaction(transaction);
-                  snapshot.data!.remove(transaction);
-                },
-                background: const DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(199, 244, 67, 54),
-                  ),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                      size: 46,
+    return Consumer<CategoryRepository>(
+      builder: (context, value, child) {
+        return FutureBuilder(
+          future: coreDatabase.transactionsList(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                padding: const EdgeInsets.only(bottom: 90),
+                itemCount: snapshot.data!.length >= _viewCount
+                    ? _viewCount
+                    : snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  // reverse the order on the list
+                  final transaction =
+                      snapshot.data![snapshot.data!.length - index - 1];
+                  return Dismissible(
+                    key: UniqueKey(),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (direction) async {
+                      await Provider.of<WalletRepository>(context,
+                              listen: false)
+                          .removeTransaction(transaction);
+                      snapshot.data!.remove(transaction);
+                    },
+                    background: const DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(199, 244, 67, 54),
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                          size: 46,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                child: TransactionCard(
-                  transaction: transaction,
-                  dialogCallback: updateDialogCallback,
-                ),
+                    child: TransactionCard(
+                      transaction: transaction,
+                      dialogCallback: updateDialogCallback,
+                    ),
+                  );
+                },
               );
-            },
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        );
       },
     );
   }
