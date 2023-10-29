1164c964b1e8b8987aee71ea9ffec109c5efa2f8
lz-coder <lzcoder@proton.me>
2023-10-29 02:22:53 -0300

    Add wallet_value_counter.dart

---
+++ lib/widgets/wallet_value_counter.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:just_finance_app/Repository/wallet_repository.dart';
+import 'package:just_finance_app/src/core_functions.dart';
+import 'package:just_finance_app/src/currency.dart';
+import 'package:provider/provider.dart';
+
+class WalletValueCounter extends StatelessWidget {
+  const WalletValueCounter({super.key, this.onTopbar = true});
+
+  final bool onTopbar;
+
+  @override
+  Widget build(BuildContext context) {
+    final totalValue = Provider.of<WalletRepository>(context).walletTotalValue;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.monetization_on_outlined,
+          size: 32,
+          color: totalValue >= 0 ? Colors.green : Colors.red,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 5, right: 20),
+          child: Text(
+              Currency(locale: getCurrentLocale(context)).show(totalValue),
+              style: TextStyle(fontSize: 22)),
+        ),
+      ],
+    );
+  }
+}
