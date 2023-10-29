08d70fddfdbed5e2c8e48e346a346d1174257244
lz-coder <lzcoder@proton.me>
2023-10-29 12:36:32 -0300

    Make use of WalletValueCounter

--- lib/widgets/topbar.dart
+++ lib/widgets/topbar.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:just_finance_app/Repository/wallet_repository.dart';
 import 'package:just_finance_app/pages/config_page.dart';
-import 'package:just_finance_app/src/core_functions.dart';
-import 'package:just_finance_app/src/currency.dart';
 import 'package:just_finance_app/widgets/topbar_tabbar.dart';
-import 'package:provider/provider.dart';
+import 'package:just_finance_app/widgets/wallet_value_counter.dart';
 
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
   final Function changeTabCallback;
@@ -18,22 +15,8 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
   final Size preferredSize = const Size.fromHeight(100);
   @override
   Widget build(BuildContext context) {
-    final totalValue = Provider.of<WalletRepository>(context).walletTotalValue;
     return AppBar(
-      title: Row(
-        children: [
-          Icon(
-            Icons.monetization_on_outlined,
-            size: 32,
-            color: totalValue >= 0 ? Colors.green : Colors.red,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 5, right: 20),
-            child: Text(
-                Currency(locale: getCurrentLocale(context)).show(totalValue)),
-          ),
-        ],
-      ),
+      title: const WalletValueCounter(),
       actions: [
         GestureDetector(
           onTap: () => Navigator.push(context,
