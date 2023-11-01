0dd91d68507a0f4f0b26d1c65f76204d54fad81c
lz-coder <lzcoder@proton.me>
2023-11-01 18:30:20 -0300

    Better on small screen devices

--- lib/widgets/transaction_dialog.dart
+++ lib/widgets/transaction_dialog.dart
@@ -49,169 +49,172 @@ class _TransactionDialogState extends State<TransactionDialog> {
 
     return Consumer<CategoryRepository>(builder: (context, value, child) {
       return Dialog(
-        child: Container(
-          height: 400,
-          width: 400,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          decoration: BoxDecoration(
-            color: widget.income
-                ? const Color.fromARGB(155, 39, 80, 51)
-                : const Color.fromARGB(155, 80, 47, 39),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                widget.transaction != null
-                    ? AppLocalizations.of(context)!.transactionCardEditing
-                    : AppLocalizations.of(context)!.transactionCardNew,
-                style: const TextStyle(fontSize: 18),
-              ),
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                    label: Text(AppLocalizations.of(context)!
-                        .transactionDialogTitleLabel)),
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                controller: valueController,
-                decoration: InputDecoration(
-                    label: Text(AppLocalizations.of(context)!
-                        .transactionDialogValueLabel)),
-              ),
-              FutureBuilder(
-                future: widget.income
-                    ? coreDatabase.incomeCategories()
-                    : coreDatabase.expenseCategories(),
-                builder: (context, snapshot) {
-                  final List<DropdownMenuEntry<Category>> categoryEntries = [];
-                  if (snapshot.hasData) {
-                    //selectedCategorie = snapshot.data![0];
-                    for (final categorie in snapshot.data!) {
-                      categoryEntries.add(DropdownMenuEntry(
-                          value: categorie, label: categorie.name));
-                    }
-
-                    if (widget.transaction != null &&
-                        selectedCategory == null) {
-                      for (Category category in snapshot.data!) {
-                        if (category.id == widget.transaction!.category) {
-                          selectedCategory = category;
-                        }
+        child: SingleChildScrollView(
+          child: Container(
+            height: 400,
+            width: 400,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+              color: widget.income
+                  ? const Color.fromARGB(155, 39, 80, 51)
+                  : const Color.fromARGB(155, 80, 47, 39),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  widget.transaction != null
+                      ? AppLocalizations.of(context)!.transactionCardEditing
+                      : AppLocalizations.of(context)!.transactionCardNew,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                      label: Text(AppLocalizations.of(context)!
+                          .transactionDialogTitleLabel)),
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: valueController,
+                  decoration: InputDecoration(
+                      label: Text(AppLocalizations.of(context)!
+                          .transactionDialogValueLabel)),
+                ),
+                FutureBuilder(
+                  future: widget.income
+                      ? coreDatabase.incomeCategories()
+                      : coreDatabase.expenseCategories(),
+                  builder: (context, snapshot) {
+                    final List<DropdownMenuEntry<Category>> categoryEntries =
+                        [];
+                    if (snapshot.hasData) {
+                      //selectedCategorie = snapshot.data![0];
+                      for (final categorie in snapshot.data!) {
+                        categoryEntries.add(DropdownMenuEntry(
+                            value: categorie, label: categorie.name));
                       }
-                    } else if (widget.transaction == null &&
-                        selectedCategory == null) {
-                      preSelectedCategory = snapshot.data![0];
-                    } else {
-                      if (!snapshot.data!.contains(selectedCategory)) {
-                        selectedCategory = snapshot.data![0];
-                      }
-                    }
 
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        DropdownMenu<Category>(
-                          width: 220,
-                          initialSelection:
-                              preSelectedCategory ?? selectedCategory,
-                          controller: categoriesController,
-                          label: Text(AppLocalizations.of(context)!
-                              .transactionDialogCategorieLabel),
-                          dropdownMenuEntries: categoryEntries,
-                          onSelected: (Category? categorie) {
-                            selectedCategory = categorie;
-                          },
-                        ),
-                        SizedBox(
-                          width: 64,
-                          height: 48,
-                          child: OutlinedButton(
-                            onPressed: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const CategoriesEditor()),
-                            ),
-                            child: const Icon(Icons.edit, size: 28),
-                          ),
-                        )
-                      ],
-                    );
-                  }
-                  return const CircularProgressIndicator();
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () async {
-                      try {
-                        if (widget.transaction == null) {
-                          late int lastTransactionId;
-                          final transactionsList =
-                              await coreDatabase.transactionsList();
-                          if (transactionsList.isNotEmpty) {
-                            lastTransactionId = transactionsList.last.id + 1;
-                          } else {
-                            lastTransactionId = 0;
+                      if (widget.transaction != null &&
+                          selectedCategory == null) {
+                        for (Category category in snapshot.data!) {
+                          if (category.id == widget.transaction!.category) {
+                            selectedCategory = category;
                           }
-                          final transaction = TransactionInfo(
-                            id: lastTransactionId,
-                            title: titleController.text.isNotEmpty
-                                ? titleController.text
-                                // ignore: use_build_context_synchronously
-                                : AppLocalizations.of(context)!
-                                    .transactionCardNew,
-                            income: widget.income ? 1 : 0,
-                            value: valueController.text.isNotEmpty
-                                ? double.parse(valueController.text)
-                                : 1,
-                            category: selectedCategory != null
-                                ? selectedCategory!.id
-                                : preSelectedCategory!.id,
-                            categoryName: selectedCategory != null
-                                ? selectedCategory!.name
-                                : preSelectedCategory!.name,
-                            year: DateTime.now().year,
-                            month: DateTime.now().month,
-                            date: DateFormat.yMd().format(DateTime.now()),
-                          );
-                          // ignore: use_build_context_synchronously
-                          await Provider.of<WalletRepository>(context,
-                                  listen: false)
-                              .insertTransaction(transaction);
-                        } else {
-                          widget.transaction!.title = titleController.text;
-                          widget.transaction!.value =
-                              double.parse(valueController.text);
-                          widget.transaction!.category = selectedCategory!.id;
-                          widget.transaction!.categoryName =
-                              selectedCategory!.name;
-                          await Provider.of<WalletRepository>(context,
-                                  listen: false)
-                              .updateTransaction(widget.transaction!);
                         }
-                        if (context.mounted) Navigator.of(context).pop();
-                      } catch (err) {
-                        debugPrint('error: ${err.toString()}');
+                      } else if (widget.transaction == null &&
+                          selectedCategory == null) {
+                        preSelectedCategory = snapshot.data![0];
+                      } else {
+                        if (!snapshot.data!.contains(selectedCategory)) {
+                          selectedCategory = snapshot.data![0];
+                        }
                       }
-                    },
-                    child: Text(widget.transaction == null
-                        ? AppLocalizations.of(context)!
-                            .transactionDialogAddLabel
-                        : MaterialLocalizations.of(context).saveButtonLabel),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: Text(
-                        MaterialLocalizations.of(context).cancelButtonLabel),
-                  ),
-                ],
-              ),
-            ],
+
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          DropdownMenu<Category>(
+                            width: 180,
+                            initialSelection:
+                                preSelectedCategory ?? selectedCategory,
+                            controller: categoriesController,
+                            label: Text(AppLocalizations.of(context)!
+                                .transactionDialogCategorieLabel),
+                            dropdownMenuEntries: categoryEntries,
+                            onSelected: (Category? categorie) {
+                              selectedCategory = categorie;
+                            },
+                          ),
+                          SizedBox(
+                            width: 48,
+                            height: 48,
+                            child: OutlinedButton(
+                              onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const CategoriesEditor()),
+                              ),
+                              child: const Icon(Icons.edit, size: 28),
+                            ),
+                          )
+                        ],
+                      );
+                    }
+                    return const CircularProgressIndicator();
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          if (widget.transaction == null) {
+                            late int lastTransactionId;
+                            final transactionsList =
+                                await coreDatabase.transactionsList();
+                            if (transactionsList.isNotEmpty) {
+                              lastTransactionId = transactionsList.last.id + 1;
+                            } else {
+                              lastTransactionId = 0;
+                            }
+                            final transaction = TransactionInfo(
+                              id: lastTransactionId,
+                              title: titleController.text.isNotEmpty
+                                  ? titleController.text
+                                  // ignore: use_build_context_synchronously
+                                  : AppLocalizations.of(context)!
+                                      .transactionCardNew,
+                              income: widget.income ? 1 : 0,
+                              value: valueController.text.isNotEmpty
+                                  ? double.parse(valueController.text)
+                                  : 1,
+                              category: selectedCategory != null
+                                  ? selectedCategory!.id
+                                  : preSelectedCategory!.id,
+                              categoryName: selectedCategory != null
+                                  ? selectedCategory!.name
+                                  : preSelectedCategory!.name,
+                              year: DateTime.now().year,
+                              month: DateTime.now().month,
+                              date: DateFormat.yMd().format(DateTime.now()),
+                            );
+                            // ignore: use_build_context_synchronously
+                            await Provider.of<WalletRepository>(context,
+                                    listen: false)
+                                .insertTransaction(transaction);
+                          } else {
+                            widget.transaction!.title = titleController.text;
+                            widget.transaction!.value =
+                                double.parse(valueController.text);
+                            widget.transaction!.category = selectedCategory!.id;
+                            widget.transaction!.categoryName =
+                                selectedCategory!.name;
+                            await Provider.of<WalletRepository>(context,
+                                    listen: false)
+                                .updateTransaction(widget.transaction!);
+                          }
+                          if (context.mounted) Navigator.of(context).pop();
+                        } catch (err) {
+                          debugPrint('error: ${err.toString()}');
+                        }
+                      },
+                      child: Text(widget.transaction == null
+                          ? AppLocalizations.of(context)!
+                              .transactionDialogAddLabel
+                          : MaterialLocalizations.of(context).saveButtonLabel),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: Text(
+                          MaterialLocalizations.of(context).cancelButtonLabel),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       );
