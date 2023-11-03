37401ab50d5b910ca260209d855da2e0058b6a27
lz-coder <lzcoder@proton.me>
2023-11-03 18:07:17 -0300

    rewritten using FutureBuilder

--- lib/widgets/year_drawer_button.dart
+++ lib/widgets/year_drawer_button.dart
@@ -33,63 +33,74 @@ class _YearDrawerButtonState extends State<YearDrawerButton> {
     final int currentMonth =
         Provider.of<DateRepository>(context, listen: false).currentMonth!;
 
-    if (currentYear == widget.year.year) {
-      isYearActive = true;
-      //dateProvider.selectedYear = widget.year.year;
-    }
+    List<Month> months = [];
 
-    List<Month>? months;
-
-    return Column(
-      children: [
-        ListTile(
-          leading: Icon(
-            isYearActive ? Icons.arrow_drop_up : Icons.arrow_drop_down,
-            size: 28,
-          ),
-          title: Text(
-            '${widget.year.year}',
-            style: const TextStyle(fontSize: 15),
-          ),
-          onTap: () {
-            activeYear();
-          },
-        ),
-        if (isYearActive)
-          FutureBuilder(
-            future: coreDatabase.getMonthsFromYear(
-                year: widget.year.year, context: context),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                months = snapshot.data!;
-                return Align(
-                  child: SizedBox(
-                    width: 200,
-                    height: 60 * months!.length.toDouble(),
-                    child: ListView.builder(
-                      itemCount: months!.length,
-                      itemBuilder: (context, index) {
-                        final month = months![index];
-                        return ListTile(
-                          leading: month.monthNumber == currentMonth
-                              ? const Icon(Icons.arrow_right)
-                              : null,
-                          title: Text(month.monthName!),
-                          onTap: () {
-                            dateProvider.selectedMonth = month.monthNumber;
-                            dateProvider.selectedYear = widget.year.year;
-                          },
-                        );
-                      },
-                    ),
+    return FutureBuilder(
+      future: coreDatabase.getMonthsFromYear(
+          year: widget.year.year, context: context),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          months = snapshot.data!;
+        }
+        return Column(
+          children: [
+            ListTile(
+              titleAlignment: ListTileTitleAlignment.center,
+              leading: months.isNotEmpty
+                  ? Icon(
+                      isYearActive
+                          ? Icons.arrow_drop_up
+                          : Icons.arrow_drop_down,
+                      size: 32,
+                    )
+                  : null,
+              title: Text(
+                '${widget.year.year}',
+                style: const TextStyle(fontSize: 15),
+              ),
+              trailing: Text(widget.year.year == currentYear ? 'Current' : ''),
+              onTap: () {
+                if (months.isEmpty) {
+                  dateProvider.selectedMonth = null;
+                  dateProvider.selectedYear = null;
+                  Navigator.pop(context);
+                } else {
+                  activeYear();
+                }
+              },
+            ),
+            if (isYearActive)
+              Align(
+                child: SizedBox(
+                  width: 250,
+                  height: 60 * months.length.toDouble(),
+                  child: ListView.builder(
+                    itemCount: months.length,
+                    itemBuilder: (context, index) {
+                      final month = months[index];
+                      return ListTile(
+                        titleAlignment: ListTileTitleAlignment.center,
+                        leading: month.monthNumber == dateProvider.selectedMonth
+                            ? const Icon(
+                                Icons.square_rounded,
+                                size: 14,
+                              )
+                            : null,
+                        title: Text(
+                            '${month.monthName!} (${month.monthNumber == currentMonth ? "current" : ""})'),
+                        onTap: () {
+                          dateProvider.selectedMonth = month.monthNumber;
+                          dateProvider.selectedYear = widget.year.year;
+                          Navigator.pop(context);
+                        },
+                      );
+                    },
                   ),
-                );
-              } else {
-                return const Text('');
-              }
-            },
-          ),
-      ],
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
