71a0637962711289d271ea4c4e93b4d38f40ae07
lz-coder <lzcoder@proton.me>
2023-11-13 00:10:29 -0300

    Add AppColors

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color incomeColor = Color.fromARGB(204, 83, 117, 76);
+  static const Color expenseColor = Color.fromARGB(204, 117, 76, 76);
+  static const Color incomeChartColor = Color.fromARGB(255, 0, 150, 62);
+  static const Color expenseChartColor = Color.fromARGB(255, 255, 69, 69);
+}
